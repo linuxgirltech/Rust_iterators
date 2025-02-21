62813119e7c87af067af75727bc8be1605ede3e1
Fae Hamilton-Raine <faehamiltonraine@gmail.com>
2025-02-21 15:15:07 -0700

    Added to_uppercase function

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,34 @@ fn print_elements(elements: &[String])  {
         .map(|el| format!("{} {}", el, el))
         .for_each(|el| println!("{}", el));
 }
- 
+
+fn shorten_strings(elements: &mut [String]) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
+
+fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements.iter().map(|el| el.to_uppercase()).collect()
+}
+
+fn move_elements() {
+    
+}
 
 fn main() {
-   let colors = vec![
+   let mut colors = vec![
     String::from("red"),
+    String::from("orange"),
+    String::from("yellow"),
     String::from("green"),
-    String::from("blue")
+    String::from("blue"),
+    String::from("indigo"),
+    String::from("violet")
    ];
 
-    print_elements(&colors);
+    // print_elements(&colors);
+    // shorten_strings(&mut colors);
+    // println!("{:#?}", colors);
+    // let uppercased = to_uppercase(&colors);
+    // println!("{:#?}", uppercased);
 }
 
